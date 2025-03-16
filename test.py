2f23a7cea7c0cc07d61a68792005bf7a7bc4532e
Sanjay-Saravanan <sanjay.saravanan0204@gmail.com>
2025-03-17 00:10:43 +0530

    Diarization added using pyannote v-3.1

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ emotion_analyzer = pipeline("text-classification", model="michellejieli/emotion_
 # Initialize the speaker diarization pipeline
 diarization_pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-3.1",
-    use_auth_token="UR ACCESS TOKEN"
+    #use_auth_token="UR ACCESS TOKEN"
 )
 diarization_pipeline.to(torch.device(device))
 
